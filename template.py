5dd2bf4e84862213df61e3731f8413b55f8b45ab
wwhere <guillermo.heras@gmail.com>
2024-12-02 08:42:33 +0000

    Add Day 2

---
+++ template.py
@@ -0,0 +1,14 @@
+from aocTools import getLines
+dayData = "2a"
+
+
+def process(fileName):
+    count = 0
+    for line in getLines(fileName):
+        continue
+
+    return count
+
+
+assert process(f'd{dayData}.ex.data') == 31
+process(f'd{dayData}.data')
