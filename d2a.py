045ec84fef212b67ff0dd94a50ff7b029f7569cc
wwhere <guillermo.heras@gmail.com>
2024-12-02 08:42:33 +0000

    Add Day 2

---
+++ d2a.py
@@ -0,0 +1,40 @@
+from aocTools import getLines
+
+day = "2a"
+
+validRange = range(1, 4)
+
+
+def process(fileName):
+    count = 0
+    for report in getLines(fileName):
+        levels = map(int, report.split())
+        safe = True
+        goingUp = False
+        lastLevel = 0
+        index = 0
+        for index, level in enumerate(levels):
+            if index == 0:
+                lastLevel = level
+                continue
+            diff = level - lastLevel
+            if index == 1:
+                if diff == 0 or abs(diff) not in validRange:
+                    safe = False
+                    break
+                goingUp = diff > 0
+                lastLevel = level
+                continue
+            if diff == 0 or goingUp and diff < 0 or not goingUp and diff > 0 or abs(diff) not in validRange:
+                safe = False
+                break
+            lastLevel = level
+        if safe:
+            count += 1
+
+    print(f'{fileName}: {count}')
+    return count
+
+
+assert process(f'd{day}.ex.data') == 2
+process(f'd{day}.data')
---
+++ d2b.py
@@ -0,0 +1,61 @@
+from aocTools import getLines
+
+day = "2a"
+
+validRange = range(1, 4)
+
+
+def checkReport(report, skip=False, skipIndex=0):
+    levels = map(int, report.split())
+    safe = True
+    goingUp = False
+    lastLevel = 0
+    checkAgain = False
+    indexesToSkip = (0, 0)
+    for index, level in enumerate(levels):
+        if skip and skipIndex == index:
+            continue
+        if index == 0 or index == 1 and skip and skipIndex == 0:
+            lastLevel = level
+            continue
+        diff = level - lastLevel
+        if index == 1 or index == 2 and skip and skipIndex < 2:
+            if diff == 0 or abs(diff) not in validRange:
+                if not skip:
+                    checkAgain = True
+                    indexesToSkip = (index-1, index)
+                    break
+                safe = False
+                break
+            goingUp = diff > 0
+            lastLevel = level
+            continue
+        if diff == 0 or goingUp and diff < 0 or not goingUp and diff > 0 or abs(diff) not in validRange:
+            if not skip:
+                checkAgain = True
+                indexesToSkip = (index-1, index)
+                break
+            safe = False
+            break
+        lastLevel = level
+
+    if checkAgain:
+        safe = checkReport(report, True, indexesToSkip[0]) or checkReport(
+            report, True, indexesToSkip[1]) or checkReport(report, True, 0)
+
+    return safe
+
+
+def process(fileName):
+    count = 0
+    for report in getLines(fileName):
+
+        if checkReport(report):
+            count += 1
+
+    print(f'{fileName}: {count}')
+    return count
+
+
+assert process(f'd{day}.ex.data') == 4
+process(f'd{day}.data')
