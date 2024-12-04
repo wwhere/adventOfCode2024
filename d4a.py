5475d9750306abd8886ecc3f2321a17c6f45b13c
wwhere <guillermo.heras@gmail.com>
2024-12-04 07:49:58 +0000

    Day 4

---
+++ d4a.py
@@ -0,0 +1,71 @@
+from aocTools import getLines
+dayData = "4a"
+
+
+def checkPossibility(grid, startX, startY, xTransformation, yTransformation, desc):
+    valid = True
+    for i, checkLetter in enumerate('XMAS'):
+        x = xTransformation(startX, i)
+        y = yTransformation(startY, i)
+        if grid[y][x] != checkLetter:
+            valid = False
+            break
+    return valid
+
+
+def process(fileName):
+    count = 0
+    gridLines = []
+    for line in getLines(fileName):
+        gridLines.append(line)
+
+    for y, line in enumerate(gridLines):
+        for x, letter in enumerate(line):
+            if letter != 'X':
+                continue
+            if y >= 3:
+                # North
+                if checkPossibility(gridLines, x, y, lambda x, i: x, lambda y, i: y-i, 'n'):
+                    count += 1
+
+            if y <= len(gridLines)-4:
+                # South
+                if checkPossibility(gridLines, x, y, lambda x, i: x, lambda y, i: y+i, 's'):
+                    count += 1
+
+            if x >= 3:
+                # West
+                if checkPossibility(gridLines, x, y, lambda x, i: x-i, lambda y, i: y, 'w'):
+                    count += 1
+
+            if x <= len(line)-4:
+                # East
+                if checkPossibility(gridLines, x, y, lambda x, i: x+i, lambda y, i: y, 'e'):
+                    count += 1
+
+            if y >= 3 and x >= 3:
+                # North West
+                if checkPossibility(gridLines, x, y, lambda x, i: x-i, lambda y, i: y-i, 'nw'):
+                    count += 1
+
+            if y >= 3 and x <= len(line)-4:
+                # North East
+                if checkPossibility(gridLines, x, y, lambda x, i: x+i, lambda y, i: y-i, 'ne'):
+                    count += 1
+
+            if y <= len(gridLines)-4 and x >= 3:
+                # South West
+                if checkPossibility(gridLines, x, y, lambda x, i: x-i, lambda y, i: y+i, 'sw'):
+                    count += 1
+
+            if y <= len(gridLines)-4 and x <= len(line)-4:
+                # South East
+                if checkPossibility(gridLines, x, y, lambda x, i: x+i, lambda y, i: y+i, 'se'):
+                    count += 1
+
+    print(f'{fileName}: {count}')
+    return count
+
+
+assert process(f'd{dayData}.ex.data') == 18
+process(f'd{dayData}.data')
---
+++ d4b.py
@@ -0,0 +1,34 @@
+from aocTools import getLines
+dayData = "4a"
+
+
+def checkPossibility(grid, x, y):
+    lookFor = ['M', 'S']
+    nw = grid[y-1][x-1]
+    ne = grid[y-1][x+1]
+    sw = grid[y+1][x-1]
+    se = grid[y+1][x+1]
+
+    return all([nw in lookFor, ne in lookFor, sw in lookFor, se in lookFor, nw != se, ne != sw])
+
+
+def process(fileName):
+    count = 0
+    gridLines = []
+    for line in getLines(fileName):
+        gridLines.append(line)
+
+    for y, line in enumerate(gridLines):
+        for x, letter in enumerate(line):
+            if letter != 'A':
+                continue
+            if y >= 1 and y <= len(gridLines)-2 and x >= 1 and x <= len(line)-2:
+                if checkPossibility(gridLines, x, y):
+                    count += 1
+
+    print(f'{fileName}: {count}')
+    return count
+
+
+assert process(f'd{dayData}.ex.data') == 9
+process(f'd{dayData}.data')
